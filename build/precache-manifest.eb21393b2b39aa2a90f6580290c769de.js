03af46333296e992bdf81b27f77d73eeafe3eb81
pepe199111 <piotrpustul1@gmail.com>
2020-09-04 12:30:56 +0100

    gh-pages

---
+++ build/precache-manifest.eb21393b2b39aa2a90f6580290c769de.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00e72aaf049cbc75a80702cc7ee2136c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d451d50795dce9de209c",
+    "url": "/static/css/2.afd40c00.chunk.css"
+  },
+  {
+    "revision": "44a48489cd13aa73a63f",
+    "url": "/static/css/main.5a2f0a91.chunk.css"
+  },
+  {
+    "revision": "d451d50795dce9de209c",
+    "url": "/static/js/2.79a8e4cd.chunk.js"
+  },
+  {
+    "revision": "f88b36470b8b650da4a18244e35dd001",
+    "url": "/static/js/2.79a8e4cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44a48489cd13aa73a63f",
+    "url": "/static/js/main.f45d6fff.chunk.js"
+  },
+  {
+    "revision": "8586da09a64e020a7b31",
+    "url": "/static/js/runtime-main.2fc72b05.js"
+  },
+  {
+    "revision": "59706e22e75de7fa98f96205b230013f",
+    "url": "/static/media/about-app-1.59706e22.jpg"
+  },
+  {
+    "revision": "54e8d13985ecf8fcb67b9bf7c15e16df",
+    "url": "/static/media/about-app-2.54e8d139.jpg"
+  },
+  {
+    "revision": "257e88d91580f167d1feafb834df6a00",
+    "url": "/static/media/about-app-3.257e88d9.jpg"
+  },
+  {
+    "revision": "4c1ae61a1869fb09b0b310fb7390b456",
+    "url": "/static/media/about-author.4c1ae61a.jpg"
+  },
+  {
+    "revision": "75546cf7c9b81ae93528a2d9756f5977",
+    "url": "/static/media/arrow-next.75546cf7.svg"
+  },
+  {
+    "revision": "6789d9ac83ed220ecf0f24c46900dd64",
+    "url": "/static/media/arrow-previous.6789d9ac.svg"
+  },
+  {
+    "revision": "baa58c3c16b76d6e733e5f658ec2a3f8",
+    "url": "/static/media/bcg-app.baa58c3c.png"
+  }
+]);
